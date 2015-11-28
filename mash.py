3aee5057a9afa5f7e3489dcc35925dc9eed8377e
Kadie16 <kadie@berkeley.edu>
2015-11-27 22:36:02 -0800

    fixed numbers

--- mash.py
+++ mash.py
@@ -20,25 +20,27 @@ def cat_query():
 	cat = raw_input("Type a Category: ");
 	print "Cool! ", cat, " is a great category."
 	option = raw_input("Do you want to keep it? (yes/no)");
-	def option_query(cat): 
+	def option_query(cat, i): 
 		print "What are the options? Type done when you're finished"
-		i = 1
 		options = []
 		cat = (cat, options)
 		option = raw_input("Option " + str(i) + ': '); 
-		while option != "done":
+		if option.lower() not in ("done"):
 			newCat = (cat, options + [option])
 			i+=1
 			return newCat
+			option_query(newCat, i)
 			
 
-	if option != "no":
-		while option!="done":
-			cat2 = option_query(cat)
-	end = len(cat2) - 1
-	print "ok, " + cat2[0] + " can be: "
-	for c in cat2[1]:
-		print cat2[c]
+	if option not in ("no", "done"):
+		i = 1
+		while option.lower() !="done":
+			cat2 = option_query(cat, i)
+			i += 1
+		end = len(cat2) - 1
+		print "ok, " + cat2[0] + " can be: "
+		for c in cat2[1]:
+			print cat2[c]
 
 cat_query()
 
